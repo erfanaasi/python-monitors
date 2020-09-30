c4932e7d9d38c664b8200c5133bed3b5ff5a5c30
erfanaasi <47225763+erfanaasi@users.noreply.github.com>
2020-09-30 09:44:30 -0400

    Update regexp.py

--- monitors/regexp.py
+++ monitors/regexp.py
@@ -1,9 +1,5 @@
 from antlr4 import *
 
-# from monitors.parser.RegExpLexer import RegExpLexer
-# from monitors.parser.RegExpParser import RegExpParser
-# from monitors.parser.RegExpVisitor import RegExpVisitor
-
 from RegExpLexer import RegExpLexer
 from RegExpParser import RegExpParser
 from RegExpVisitor import RegExpVisitor
