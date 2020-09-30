2683339b432f3a2ed34ab12b7f6b44e9616fe85f
erfanaasi <47225763+erfanaasi@users.noreply.github.com>
2020-09-30 10:47:07 -0400

    Update mtl.py

--- monitors/mtl.py
+++ monitors/mtl.py
@@ -1,9 +1,9 @@
 from antlr4 import *
 import ctx
 
-from PastMTLLexer import PastMTLLexer
-from PastMTLParser import PastMTLParser
-from PastMTLVisitor import PastMTLVisitor
+from monitors.parser.PastMTLLexer import PastMTLLexer
+from monitors.parser.PastMTLParser import PastMTLParser
+from monitors.parser.PastMTLVisitor import PastMTLVisitor
 
 import intervals
 
